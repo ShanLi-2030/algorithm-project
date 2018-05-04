b907ef0f8c71302e1b60b85043a6f70cb994690d
ShanLi-2030 <35753554+ShanLi-2030@users.noreply.github.com>
2018-05-03 21:38:25 -0600

    finished-ShanLi&LucasManker

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//Shan Li and Lucas Manker
+#include "func.h"
+#include "test.h"
+
+
+using namespace std;
+
+int main() {
+	testQuickSort();
+	testMergeSort();
+	testBinSearch();
+	testHash();
+	testBloom();
+	return 0;
+}
